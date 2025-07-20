e68a543dbf69e121a66c1f85d84fc1a4e64085e7
Indrawan <indrawanismail0@gmail.com>
2025-07-20 22:53:04 +0800

    form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import Header from './components/Header' 
 import BookList from './components/BookList'
 import { useEffect, useState } from 'react'
+import AddBookForm from './components/AddBookForm'
 
 function App() {
 
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState(() => {
+    const savedBook = localStorage.getItem('books')
+    return savedBook ? JSON.parse(savedBook) : []
+  })
 
-// pr tambahkan styling sebagus mungkin
-// dan tambahkan gambar buku
   useEffect(() => {
-    const sampleBooks = [
-        {title:"Hujan", author: "Tere Liye"},
-      {title:"Atomic Habits", author: "James Clear"},
-        {title:"How to Wins Friend", author: "Dale Carneige"}
-    ]
-    setBooks(sampleBooks)
-  },[])
+    localStorage.setItem('books', JSON.stringify(books))
+  },[books])
+// pr tambahkan styling sebagus mungkin
+// // dan tambahkan gambar buku
+//   useEffect(() => {
+//     const sampleBooks = [
+//         {title:"Hujan", author: "Tere Liye"},
+//       {title:"Atomic Habits", author: "James Clear"},
+//         {title:"How to Wins Friend", author: "Dale Carneige"}
+//     ]
+//     setBooks(sampleBooks)
+//   },[])
+
+  const AddBook = (book) => {
+    setBooks([...books, book])
+  }
 
   return (
   <>
     <div>
     <Header />
     <BookList books={books}/>
+    <AddBookForm 
+    onAdd={AddBook}
+    />
     </div>
     </>
   )
