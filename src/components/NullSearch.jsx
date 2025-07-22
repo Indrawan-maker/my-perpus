bf7b8dfe46eb84013b22c3c526bd84a1911eb779
Indrawan <indrawanismail0@gmail.com>
2025-07-22 20:35:15 +0800

    fix err

--- src/components/NullSearch.jsx
+++ src/components/NullSearch.jsx
@@ -1,11 +1,9 @@
-import img from "../assets/n5.webp"
-
 export default function NullSearch() {
     return (
         <>
         <div className="grid justify-center items-center p-12">
         <img 
-        src={img} 
+        src="../assets/n5.webp" 
         className="w-52 object-cover m-auto"
         />
         <h2 className="text-2xl text-slate-600 m-auto font-semibold">Buku tidak ditemukan!</h2>
